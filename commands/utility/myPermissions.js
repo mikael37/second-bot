2e07879b010d9bb55306d7223df3d58ac298c9bb
mikael37 <elias.mikael@icloud.com>
2025-01-01 17:33:58 +0000

    .

--- commands/utility/myPermissions.js
+++ commands/utility/myPermissions.js
@@ -11,26 +11,13 @@ module.exports = {
     // Get the bot's permissions in the current channel
     const permissions = botMember.permissionsIn(interaction.channel);
 
-    // Define a list of low-danger permissions to filter out
-    const lowDangerPermissions = [
-      "UseExternalSounds", "SendPolls", "UseExternalApps", "UseEmbeddedActivities", 
-      "UseSoundboard", "UseExternalStickers", "SendMessagesInThreads", 
-      "CreateGuildExpressions", "CreateEvents", "SendVoiceMessages", 
-      "CreatePublicThreads", "CreatePrivateThreads", "UseVAD", "ModerateMembers", 
-      "ViewCreatorMonetizationAnalytics", "ViewGuildInsights"
-    ];
+    // Create a horizontal list with each permission in rich text format
+    const permissionList = permissions.toArray().map(permission => `\`${permission}\``).join(" ");
 
-    // Filter out low-danger permissions
-    const filteredPermissions = permissions.toArray().filter(permission => 
-      !lowDangerPermissions.includes(permission)
-    );
-
-    // Create a formatted string of the remaining permissions
-    const permissionList = filteredPermissions.length > 0
-      ? filteredPermissions.map(permission => `\`${permission}\``).join("\n")
-      : "`No permissions`";
+    // If no permissions, display a default message
+    const message = permissionList || "`No permissions`";
 
     // Send the formatted permission list as a reply
-    await interaction.reply(`The bot has the following permissions in this channel:\n${permissionList}`);
+    await interaction.reply(`The bot has the following permissions in this channel: ${message}`);
   },
 };
