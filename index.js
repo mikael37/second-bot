598050349b2f5a5e9f8f04be92ae5a685b6c30a0
mikael37 <elias.mikael@icloud.com>
2025-01-01 03:05:40 +0000

    .

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
+const { REST, Routes } = require("discord.js");
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const interactionHandler = require("./interactionHandler");
-process.noDeprecation = true;
+require("dotenv").config();
 
-const { DISCORD_TOKEN: token } = process.env;
+const { DISCORD_TOKEN: token, CLIENT_ID: clientId, GUILD_ID: guildId } = process.env;
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-  ],
-});
-
-client.commands = new Collection();
-
-// Load commands
+const commands = [];
 const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
@@ -30,7 +18,7 @@ for (const folder of commandFolders) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
     if ("data" in command && "execute" in command) {
-      client.commands.set(command.data.name, command);
+      commands.push(command.data.toJSON());
     } else {
       console.log(
         `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
@@ -39,27 +27,28 @@ for (const folder of commandFolders) {
   }
 }
 
-client.once(Events.ClientReady, () => {
-  console.log("Bot is ready.");
-});
+const rest = new REST({ version: "10" }).setToken(token);
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (interaction.isCommand()) {
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  } else {
-    // Handle interactions like button presses here
-    await interactionHandler(interaction);
+(async () => {
+  try {
+    console.log("Started clearing outdated commands.");
+
+    // Clear all commands in the guild
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: [],
+    });
+    console.log("Successfully cleared all commands.");
+
+    console.log("Started refreshing application (/) commands.");
+
+    // Re-register commands
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+
+    console.log(`Successfully reloaded ${data.length} commands.`);
+  } catch (error) {
+    console.error("Error while reloading commands:", error);
   }
-});
-
-client.login(token);
+})();
