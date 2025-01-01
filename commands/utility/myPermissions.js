b6e3476c296d52b89c6f14656a2e7c49fc9f2f20
mikael37 <elias.mikael@icloud.com>
2025-01-01 17:44:06 +0000

    .

--- commands/utility/myPermissions.js
+++ commands/utility/myPermissions.js
@@ -5,19 +5,14 @@ module.exports = {
     .setName("mypermissions")
     .setDescription("Lists all the bot's permissions in the current channel."),
   async execute(interaction) {
-    // Ensure the bot is fully available
     const botMember = await interaction.guild.members.fetchMe();
 
-    // Get the bot's permissions in the current channel
     const permissions = botMember.permissionsIn(interaction.channel);
 
-    // Create a horizontal list with each permission in rich text format
     const permissionList = permissions.toArray().map(permission => `\`${permission}\``).join(" ");
 
-    // If no permissions, display a default message
     const message = permissionList || "`No permissions`";
 
-    // Send the formatted permission list as a reply
-    await interaction.reply(`The bot has the following permissions in this channel: ${message}`);
+    await interaction.reply(`The bot has the following permissions in this channel: \n${message}`);
   },
 };
