e1290c99847d51e324322108090b9c14593a790a
mikael37 <elias.mikael@icloud.com>
2025-01-02 17:56:21 +0000

    .

--- commands/utility/syncdatabase.js
+++ commands/utility/syncdatabase.js
@@ -1,38 +1,50 @@
+const { SlashCommandBuilder } = require("discord.js");
 const { users, config } = require("../../userData");
 
-async function performSync(interaction) {
-  const guild = interaction.guild;
-  const members = await guild.members.fetch();
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("syncdatabase")
+    .setDescription("Assign roles and rename users in bulk."),
 
-  const { alliancePrefixes, allianceRoleIds, kingdomRoleId } = config;
-
-  const statusMessages = [];
-
-  for (const user of users) {
-    const member = members.get(user.discordId);
-    if (!member) {
-      statusMessages.push(`User with ID ${user.discordId} not found.`);
-      continue;
-    }
-
+  async execute(interaction) {
     try {
-      const prefix = alliancePrefixes[user.alliance] || "XX";
-      const newNickname = `[${prefix}05] ${user.inGameUsername}`;
-      await member.setNickname(newNickname);
+      // Load user data from userData.js
+      const guild = interaction.guild;
+      const members = await guild.members.fetch();
 
-      const roleId = allianceRoleIds[user.alliance];
-      if (roleId) {
-        await member.roles.add(roleId);
-        await member.roles.add(kingdomRoleId);
-        statusMessages.push(`Updated ${member.user.tag}: Renamed and assigned role "${user.alliance}".`);
-      } else {
-        statusMessages.push(`Role for alliance "${user.alliance}" not found. Skipping role assignment.`);
+      const { alliancePrefixes, allianceRoleIds, kingdomRoleId } = config;
+      const statusMessages = [];
+
+      for (const user of users) {
+        const member = members.get(user.discordId);
+        if (!member) {
+          statusMessages.push(`User with ID ${user.discordId} not found.`);
+          continue;
+        }
+
+        try {
+          const prefix = alliancePrefixes[user.alliance] || "XX";
+          const newNickname = `[${prefix}05] ${user.inGameUsername}`;
+          await member.setNickname(newNickname);
+
+          const roleId = allianceRoleIds[user.alliance];
+          if (roleId) {
+            await member.roles.add(roleId);
+            await member.roles.add(kingdomRoleId);
+            statusMessages.push(`Updated ${member.user.tag}: Renamed and assigned role "${user.alliance}".`);
+          } else {
+            statusMessages.push(`Role for alliance "${user.alliance}" not found. Skipping role assignment.`);
+          }
+        } catch (userError) {
+          console.error(`Error updating ${user.discordId}:`, userError);
+          statusMessages.push(`Failed to update user with ID ${user.discordId}.`);
+        }
       }
-    } catch (userError) {
-      console.error(`Error updating ${user.discordId}:`, userError);
-      statusMessages.push(`Failed to update user with ID ${user.discordId}.`);
-    }
-  }
 
-  await interaction.editReply(statusMessages.join("\n"));
-}
+      await interaction.editReply(statusMessages.join("\n"));
+    } catch (error) {
+      console.error("Bulk assignment failed:", error);
+      await interaction.editReply("An error occurred while assigning roles in bulk.");
+    }
+  },
+};
